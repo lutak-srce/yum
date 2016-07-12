8a32f3438d1fa11da355f92a3f71c1d2769ff9f3
Zavisa Nikac <znikac@srce.hr>
2016-07-12 16:33:56 +0200

    various changes for srce branch

--- manifests/repo/ius.pp
+++ manifests/repo/ius.pp
@@ -37,5 +37,12 @@ class yum::repo::ius (
         source   =>  'http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/ius-release-1.0-14.ius.el6.noarch.rpm',
       }
     }
+    /^7.*/: {
+      package { 'ius-release':
+        ensure   => '1.0-14.ius.el7',
+        provider => 'rpm',
+        source   =>  'http://dl.iuscommunity.org/pub/ius/stable/Redhat/7/x86_64/ius-release-1.0-14.ius.el7.noarch.rpm',
+      }
+    }
   }
 }
---
+++ manifests/repo/pgdg95.pp
@@ -0,0 +1,38 @@
+# Class: yum::repo::pgdg95
+#
+# This module manages PostgreSQL 9.5 repo files for $lsbdistrelease
+#
+class yum::repo::pgdg95 (
+  $stage   = 'yumsetup',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/pgdg-95-centos.repo' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    source  => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/pgdg-95-centos.repo",
+    require => Package['pgdg-centos95'],
+  }
+
+  # install package depending on major version
+  case $::operatingsystemrelease {
+    default: {}
+    /^6.*/: {
+      package { 'pgdg-centos95' :
+        ensure   => '9.5-2',
+        provider => 'rpm',
+        source   => 'http://yum.postgresql.org/9.5/redhat/rhel-6-x86_64/pgdg-centos95-9.5-2.noarch.rpm', }
+    }
+    /^7.*/: {
+      package { 'pgdg-centos95' :
+        ensure   => '9.5-2',
+        provider => 'rpm',
+        source   => 'http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm', }
+    }
+  }
+}
