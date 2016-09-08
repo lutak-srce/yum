caf3a7e7c94c84b948cd62de27d8610010e917f6
Jakov Sosic <jsosic@gmail.com>
2016-09-08 03:37:35 +0200

    2016-09 big sync from bitbucket

--- manifests/repo/pgdg94.pp
+++ manifests/repo/pgdg94.pp
@@ -25,15 +25,15 @@ class yum::repo::pgdg94 (
     default: {}
     /^5.*/: {
       package { 'pgdg-centos94' :
-        ensure   => '9.4-1',
+        ensure   => '9.4-2',
         provider => 'rpm',
-        source   => 'http://yum.postgresql.org/9.4/redhat/rhel-5-i386/pgdg-centos94-9.4-1.noarch.rpm', }
+        source   => 'http://yum.postgresql.org/9.4/redhat/rhel-5-i386/pgdg-centos94-9.4-2.noarch.rpm', }
     }
     /^6.*/: {
       package { 'pgdg-centos94' :
-        ensure   => '9.4-1',
+        ensure   => '9.4-2',
         provider => 'rpm',
-        source   => 'http://yum.postgresql.org/9.4/redhat/rhel-6-i386/pgdg-centos94-9.4-1.noarch.rpm', }
+        source   => 'http://yum.postgresql.org/9.4/redhat/rhel-6-i386/pgdg-centos94-9.4-2.noarch.rpm', }
     }
   }
 }
