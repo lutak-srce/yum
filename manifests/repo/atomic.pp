29411d3adf7f17ec2dd81775c0483cde0b874c18
Jakov Sosic <jsosic@gmail.com>
2016-11-23 00:36:13 +0100

    Update Atomic repo pp and erb files

--- manifests/repo/atomic.pp
+++ manifests/repo/atomic.pp
@@ -1,4 +1,5 @@
-# Class: yum::repo::atomic
+#
+# = Class: yum::repo::atomic
 #
 # This module manages ATOMIC repo files for $operatingsystemrelease
 #
@@ -11,50 +12,47 @@ class yum::repo::atomic (
 ) {
   require yum::repo::base
 
-  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY.art.txt':
-    ensure => file,
-    mode   => '0644',
-    owner  => root,
-    group  => root,
-    source => 'puppet:///modules/yum/keys/RPM-GPG-KEY.art.txt',
-  }
-  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY.atomicorp.txt':
-    ensure => file,
-    mode   => '0644',
-    owner  => root,
-    group  => root,
-    source => 'puppet:///modules/yum/keys/RPM-GPG-KEY.atomicorp.txt',
-  }
-  file { '/etc/yum.repos.d/atomic.repo' :
+  File {
     ensure  => file,
     mode    => '0644',
     owner   => root,
     group   => root,
-    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/atomic.erb"),
     require => Package['atomic-release'],
+  }
+
+  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY.art.txt':
+    source => 'puppet:///modules/yum/keys/RPM-GPG-KEY.art.txt',
+  }
+
+  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY.atomicorp.txt':
+    source => 'puppet:///modules/yum/keys/RPM-GPG-KEY.atomicorp.txt',
+  }
+
+  file { '/etc/yum.repos.d/atomic.repo' :
+    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/atomic.erb"),
   }
 
   case $::operatingsystemrelease {
     default: {}
     /^5.*/: {
       package { 'atomic-release' :
-        ensure   => '1.0-21.el5.art',
+        ensure   => present,
         provider => 'rpm',
         source   => 'http://www3.atomicorp.com/channels/atomic/centos/5/x86_64/RPMS/atomic-release-1.0-21.el5.art.noarch.rpm',
       }
     }
     /^6.*/: {
       package { 'atomic-release' :
-        ensure   => '1.0-21.el6.art',
+        ensure   => present,
         provider => 'rpm',
         source   => 'http://www6.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/atomic-release-1.0-21.el6.art.noarch.rpm',
       }
     }
     /^7.*/: {
       package { 'atomic-release' :
-        ensure   => '1.0-20.el7.art',
+        ensure   => present,
         provider => 'rpm',
-        source   => 'http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-20.el7.art.noarch.rpm',
+        source   => 'http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.el7.art.noarch.rpm',
       }
     }
   }
