c478c72174da21e506c583c4dcd330c177cfe83f
Jakov Sosic <jsosic@gmail.com>
2016-11-23 00:44:34 +0100

    Add grafana repo

---
+++ manifests/repo/grafana.pp
@@ -0,0 +1,25 @@
+#
+# = Class: yum::repo::grafana
+#
+# This module manages grafana repo files for $lsbdistrelease
+#
+class yum::repo::grafana (
+  $stage     = 'yumsetup',
+  $priority  = '1',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+) {
+  require yum::repo::base
+
+  File {
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
+  file { "/etc/yum.repos.d/grafana.repo":
+    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/grafana.erb"),
+  }
+}
