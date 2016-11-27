cb4d1f51f459e365b205c270598f3bd9720cc463
Zavisa Nikac <znikac@srce.hr>
2016-11-27 01:26:47 +0100

    various additions or changes

--- manifests/repo/puppetlabspc1.pp
+++ manifests/repo/puppetlabspc1.pp
@@ -28,19 +28,19 @@ class yum::repo::puppetlabspc1 (
     /^5.*/: {
       package { 'puppetlabs-release-pc1' :
         provider => 'rpm',
-        source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-5.noarch.rpm',
+        source   => 'http://yum.puppetlabs.com/el/5/PC1/x86_64/puppetlabs-release-pc1-1.1.0-4.el5.noarch.rpm',
       }
     }
     /^6.*/: {
       package { 'puppetlabs-release-pc1' :
         provider => 'rpm',
-        source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm',
+        source   => 'http://yum.puppetlabs.com/el/6/PC1/x86_64/puppetlabs-release-pc1-1.1.0-4.el6.noarch.rpm',
       }
     }
     /^7.*/: {
       package { 'puppetlabs-release-pc1' :
         provider => 'rpm',
-        source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm',
+        source   => 'http://yum.puppetlabs.com/el/7/PC1/x86_64/puppetlabs-release-pc1-1.1.0-4.el7.noarch.rpm',
       }
     }
   }
--- manifests/repo/rpmforge.pp
+++ manifests/repo/rpmforge.pp
@@ -26,21 +26,22 @@ class yum::repo::rpmforge (
       package {'rpmforge-release':
         ensure   => '0.5.3-1.el5.rf',
         provider => 'rpm',
-        source   => 'http://apt.sw.be/redhat/el5/en/i386/rpmforge/RPMS/rpmforge-release-0.5.3-1.el5.rf.i386.rpm',
+        source   => 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm',
       }
     }
     /^6.*/: {
       package {'rpmforge-release':
         ensure   => '0.5.3-1.el6.rf',
         provider => 'rpm',
-        source   => 'http://apt.sw.be/redhat/el6/en/i386/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.i686.rpm',
+        #source   => 'http://apt.sw.be/redhat/el6/en/i386/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.i686.rpm',
+        source   => 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm',
       }
     }
     /^7.*/: {
       package {'rpmforge-release':
         ensure   => '0.5.3-1.el7.rf',
         provider => 'rpm',
-        source   => 'http://apt.sw.be/redhat/el7/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm',
+        source   => 'http://ftp.pbone.net/mirror/dag.wieers.com/redhat/el7/en/x86_64/dag/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm',
       }
     }
   }
