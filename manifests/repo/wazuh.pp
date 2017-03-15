1ce01539528581c6dbe13eee4607662de17768fd
znikac <znikac@srce.hr>
2017-03-15 09:57:59 +0100

    add wazuh repo for OSSEC

---
+++ manifests/repo/wazuh.pp
@@ -0,0 +1,24 @@
+#
+# = Class: yum::repo::wazuh
+#
+# This module manages Wazuh repo files for $lsbdistrelease
+#
+class yum::repo::wazuh (
+  $stage     = 'yumsetup',
+  $priority  = '15',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+) {
+
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/wazuh.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/wazuh.erb"),
+  }
+
+}
