4259c46ce1a6045bbf4450c5b3f7fe75ad25399d
Jakov Sosic <jsosic@gmail.com>
2018-01-08 15:38:03 +0100

    Add Docker Community Edition yum repo

---
+++ manifests/repo/dockerce.pp
@@ -0,0 +1,24 @@
+#
+# = Class: yum::repo::dockerce
+#
+# This module manages Docker Community Edition repo files for $lsbdistrelease
+#
+class yum::repo::ius (
+  $stage     = 'yumsetup',
+  $priority  = '61',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+  $source    = false,
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/docker-ce.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template('yum/generic/docker-ce.erb'),
+  }
+
+}
