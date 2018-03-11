e3f0b9fec10479cf0ba946aad57250b9eb7a975a
Jakov Sosic <jsosic@gmail.com>
2018-03-11 19:03:16 +0100

    Add oVirt repo with dependency repos

---
+++ manifests/repo/base/ovirt.pp
@@ -0,0 +1,34 @@
+#
+# = Class: yum::repo::base::ovirt
+#
+# This module manages oVirt repo files for $operatingsystemrelease
+#
+
+# CentOS
+class yum::repo::base::ovirt (
+  $stage     = 'yumsetup',
+  $priority  = '2',
+  $version   = '4.2',
+  $exclude   = [],
+  $include   = [],
+  $testing   = false,
+){
+
+  $version_nodot = regsubst($version, '\.', '')
+
+  package { "centos-release-ovirt${version_nodot}": }
+
+  case $::operatingsystem {
+    default : {}
+    'CentOS' : {
+      file { "/etc/yum.repos.d/CentOS-oVirt-${version}.repo":
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/CentOS-oVirt.erb"),
+	require => Package["centos-release-ovirt${version_nodot}"],
+      }
+    }
+  }
+}
---
+++ manifests/repo/base/sclorh.pp
@@ -0,0 +1,31 @@
+#
+# = Class: yum::repo::base::sclorh
+#
+# This module manages sclo-rh repo files for $operatingsystemrelease
+#
+
+# CentOS
+class yum::repo::base::sclorh (
+  $stage    = 'yumsetup',
+  $priority = '2',
+  $exclude  = [],
+  $include  = [],
+){
+  require yum::repo::base
+
+  package { 'centos-release-scl-rh': }
+
+  case $::operatingsystem {
+    default : {}
+    'CentOS' : {
+      file { '/etc/yum.repos.d/CentOS-SCLo-scl-rh.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/CentOS-SCLo-scl-rh.erb"),
+        require => Package['centos-release-scl-rh'],
+      }
+    }
+  }
+}
