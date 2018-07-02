f2301be722272d5d178cccf3d5a214d752447ee9
eimamagi <emir.imamagic@gmail.com>
2018-07-02 12:29:30 +0200

    Added BeeGFS 7 repo. Added Pulp repo.

---
+++ manifests/repo/beegfs7.pp
@@ -0,0 +1,19 @@
+# Class: yum::repo::beegfs
+#
+# This module manages BeeGFS repo files for $lsbdistrelease
+#
+class yum::repo::beegfs7 (
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/beegfs7.repo' :
+    ensure => file,
+    mode   => '0644',
+    owner  => root,
+    group  => root,
+    source => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/beegfs7.repo",
+  }
+}
