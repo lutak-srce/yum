c710b7aa4d0e53fc16ca68d3eb3639ed81d66add
Jakov Sosic <jsosic@gmail.com>
2019-05-08 14:22:24 +0200

    Refactor pgdg repos
    Due to upstream changes, 9.4+ repos are being distributed with a SINGLE
    repo RPM. This PR reflects the pgdg changes.
    
    Note: instead of including ::yum::repos::pgdgXY, one can now include
    ::yum::repos::pgdg for all repos 9.4 - 12+

---
+++ manifests/repo/pgdg.pp
@@ -0,0 +1,32 @@
+#
+# = Class: yum::repo::pgdg
+#
+# This module manages PostgreSQL repo files for $lsbdistrelease
+# for versions > 9.3, while 9.3 and lower are managed by 'pgdg9[123]'
+#
+class yum::repo::pgdg {
+
+  require yum::repo::base
+
+  # install package depending on major version
+  if ( 0 + $facts['os']['release']['major'] > 6 ) {
+    $pgdg_subdir = "EL-${facts['os']['release']['major']}-${facts['os']['architecture']}"
+  } else {
+    $pgdg_subdir = "EL-${facts['os']['release']['major']}"
+  }
+
+  package { 'pgdg-redhat-repo' :
+    provider => 'rpm',
+    source   => "https://download.postgresql.org/pub/repos/yum/reporpms/${pgdg_subdir}/pgdg-redhat-repo-latest.noarch.rpm",
+  }
+
+  file { '/etc/yum.repos.d/pgdg-redhat-all.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/pgdg-redhat-all.erb"),
+    require => Package['pgdg-redhat-repo'],
+  }
+
+}
