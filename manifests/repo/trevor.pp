aac03750abe1dae544e56e1acb64af6cb4e19f4f
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-22 13:41:38 +0200

    Add TrevorBl repo

---
+++ manifests/repo/trevor.pp
@@ -0,0 +1,18 @@
+# Class: yum::repo::adesso
+#
+# This module manages Filesender repo files for $lsbdistrelease
+#
+class yum::repo::trevor(
+  $stage   = 'yumsetup',
+  $exclude  = [],
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/TrevorBl.repo' :
+    ensure => file,
+    mode   => '0644',
+    owner  => root,
+    group  => root,
+    source => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/TrevorBl.repo",
+  }
+}
