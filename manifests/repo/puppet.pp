e04710425e69f5fddd402d46380ccbecababdd2c
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-22 15:39:52 +0200

    Add support for puppet v5 & v6 repos

---
+++ manifests/repo/puppet.pp
@@ -0,0 +1,30 @@
+#
+# = Class: yum::repo::puppet
+#
+# This module manages PuppetLabs repo files for $lsbdistrelease
+#
+class yum::repo::puppet (
+  $version  = '5',
+  $priority = '1',
+  $exclude  = [],
+  $include  = [],
+  $source   = false,
+) {
+
+  require yum::repo::base
+
+  file { "/etc/yum.repos.d/puppet${version}.repo" :
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/puppet.erb"),
+    require => Package["puppet${version}-release"],
+  }
+
+  package { "puppet${version}-release" :
+    provider => 'rpm',
+    source   => "https://yum.puppetlabs.com/puppet${version}-release-el-${facts['os']['release']['major']}.noarch.rpm",
+  }
+
+}
