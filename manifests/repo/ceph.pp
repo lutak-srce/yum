96cc029d27f7fa83e5f2ec23091f5120b4388b26
Ante Jurjević <ante.jurjevic@srce.hr>
2019-07-23 11:07:00 +0200

    Add Ceph repo

---
+++ manifests/repo/ceph.pp
@@ -0,0 +1,18 @@
+class yum::repo::ceph(
+  $stage     = 'yumsetup',
+  $priority  = 1,
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+  $release   = 'nautilus',
+){
+  require yum::repo::base
+
+  file { '/etc/yum.repos.d/ceph.repo' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content =>  template("yum/${::operatingsystem}/${::operatingsystemrelease}/ceph.erb"),
+  }
+}
