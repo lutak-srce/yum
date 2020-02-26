dac5762c237359be29e6716e459c3e7d8ab414f9
eimamagi <emir.imamagic@gmail.com>
2020-02-26 08:12:21 -0500

    Add CentOS OpenStack. (#131)

---
+++ manifests/repo/base/openstack.pp
@@ -0,0 +1,33 @@
+# Class: yum::repo::base::openstack
+#
+# This module manages CentOS cloud repo files for $operatingsystemrelease
+#
+
+# Amazon
+class yum::repo::base::openstack (
+  $version   = 'stein',
+  $stage     = 'yumsetup',
+  $priority  = '1',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require ::yum::repo::base
+  require ::yum::repo::base::extras
+
+  File {
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+
+  package { "centos-release-openstack-${version}":
+    ensure => present,
+  }
+
+  file { "/etc/yum.repos.d/CentOS-OpenStack-${version}.repo":
+    ensure  => file,
+    content => template('yum/CentOS/CentOS-OpenStack.erb'),
+  }
+}
