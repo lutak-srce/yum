db777838ebab6df107b0dcd3f2825cd528e658e1
ajurjevi <26479807+ajurjevi@users.noreply.github.com>
2020-03-03 12:31:59 +0100

    Add nodesource (nodejs) repo (#142)
    Co-authored-by: Jakov Sosic <jsosic@gmail.com>

---
+++ manifests/repo/nodesource.pp
@@ -0,0 +1,29 @@
+#
+# = Class: yum::repo::nodesource
+#
+# This module manages nodejs repo files for $lsbdistrelease
+#
+class yum::repo::nodesource (
+  $stage     = 'yumsetup',
+  $priority  = '99',
+  $exclude   = [],
+  $include   = [],
+  $source    = false,
+){
+  require yum::repo::base
+
+  file {"/etc/yum.repos.d/nodesource-el${facts['os']['release']['major']}.repo":
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/nodesource.erb"),
+    require => Package['nodesource-release'],
+  }  
+
+  package { 'nodesource-release':
+    ensure   => present,
+    provider => 'rpm',
+    source   => "https://rpm.nodesource.com/pub_8.x/el/${facts['os']['release']['major']}/x86_64/nodesource-release-el${facts['os']['release']['major']}-1.noarch.rpm",
+  }  
+}
