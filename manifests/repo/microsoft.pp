a68333ab7ee794ff61aa14db0f18717683e56a4a
eimamagi <emir.imamagic@gmail.com>
2020-03-03 03:41:58 -0500

    Add Microsoft repo. (#139)

---
+++ manifests/repo/microsoft.pp
@@ -0,0 +1,21 @@
+# Class: yum::repo::microsoft
+#
+# This module manages Microsoft repo files
+#
+class yum::repo::microsoft (
+  $stage     = 'yumsetup',
+  $priority  = 99,
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require yum::repo::base
+
+  file {  '/etc/yum.repos.d/microsoft-prod.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${facts['os']['name']}/microsoft-prod.erb"),
+  }
+}
