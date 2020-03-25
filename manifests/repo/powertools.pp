cbf32929c629aa7d707f768a10c954c548637abb
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-03-25 15:08:58 +0100

    Add PowerTools repo

---
+++ manifests/repo/powertools.pp
@@ -0,0 +1,27 @@
+# Class: yum::repo::base::powertools
+#
+# This module manages Extras repo files for $operatingsystemrelease
+#
+
+# CentOS
+class yum::repo::powertools (
+  $priority  = '2',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require yum::repo::base
+
+  case $::operatingsystem {
+    default : {}
+    'CentOS' : {
+      file { '/etc/yum.repos.d/CentOS-PowerTools.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/CentOS-PowerTools.erb"),
+      }
+    }
+  }
+}
