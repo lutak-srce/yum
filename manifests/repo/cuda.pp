293e2e6c253a4218a200e021e41996261a5c30a7
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-03-30 15:11:51 +0200

    Add support for CentOS 7 for CUDA repo

--- manifests/repo/cuda.pp
+++ manifests/repo/cuda.pp
@@ -9,23 +9,38 @@ class yum::repo::cuda (
   $include   = [],
   $debuginfo = false,
 ) {
-  file { '/etc/yum.repos.d/cuda.repo':
-    ensure  => file,
-    mode    => '0644',
-    owner   => root,
-    group   => root,
-    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/cuda.erb"),
-    require => Package['cuda-repo-rhel6'],
-  }
 
   # install package depending on major version
   case $::operatingsystemrelease {
     default: {}
     /^6.*/: {
+      file { '/etc/yum.repos.d/cuda.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/cuda.erb"),
+        require => Package['cuda-repo-rhel6'],
+      }
       package { 'cuda-repo-rhel6':
+        ensure   => '8.0.44-1',
+        provider => 'rpm',
+        source   => 'http://developer.download.nvidia.com/compute/cuda/repos/rhel6/x86_64/cuda-repo-rhel6-8.0.44-1.x86_64.rpm',
+      }
+    }
+    /^7.*/: {
+      file { '/etc/yum.repos.d/cuda.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/cuda.erb"),
+        require => Package['cuda-repo-rhel7'],
+      }
+      package { 'cuda-repo-rhel7':
         ensure   => present,
         provider => 'rpm',
-        source   => 'http://developer.download.nvidia.com/compute/cuda/repos/rhel6/x86_64/cuda-repo-rhel6-5.5-0.x86_64.rpm',
+        source   => 'https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm',
       }
     }
   }
