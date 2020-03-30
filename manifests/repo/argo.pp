054cc0b83212790b29f80f589a51132d78d522ad
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-03-30 14:25:05 +0200

    Add ARGO repo

---
+++ manifests/repo/argo.pp
@@ -0,0 +1,24 @@
+#
+# = Class: yum::repo::argo
+#
+# This module manages ARGO repo files for $lsbdistrelease
+#
+class yum::repo::argo (
+  $priority  = '99',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+  $type      = 'prod',
+){
+
+  require ::yum::repo::base
+
+  file { '/etc/yum.repos.d/argo.repo':
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/argo.erb"),
+  }
+
+}
