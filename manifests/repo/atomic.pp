300fede8d35291fa6efc9a018e627e9b4c060a3b
Jakov Sosic <jsosic@gmail.com>
2020-07-07 16:07:36 +0200

    Add support for Atomic repo to EL8

--- manifests/repo/atomic.pp
+++ manifests/repo/atomic.pp
@@ -54,5 +54,12 @@ class yum::repo::atomic (
         source   => 'http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/atomic-release-1.0-21.el7.art.noarch.rpm',
       }
     }
+    /^8.*/: {
+      package { 'atomic-release' :
+        ensure   => present,
+        provider => 'rpm',
+        source   => 'https://www6.atomicorp.com/channels/atomic/centos/8/x86_64/RPMS/atomic-release-1.0-21.el8.art.noarch.rpm',
+      }
+    }
   }
 }
