3100e0508c938534f79521e5209a64b503e32968
eimamagi <emir.imamagic@gmail.com>
2020-09-14 21:19:30 +0200

    Add Zabbix frontend repository.

---
+++ manifests/repo/zabbix/frontend.pp
@@ -0,0 +1,29 @@
+# Class: yum::repo::zabbix::frontend
+#
+# This module manages Zabbix repo files for $operatingsystemrelease
+#
+
+# CentOS
+class yum::repo::zabbix::frontend (
+  $priority  = '1',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+){
+  require yum::repo::zabbix
+
+  $version = $yum::repo::zabbix::version
+
+  case $::operatingsystem {
+    default : {}
+    'CentOS' : {
+      file { '/etc/yum.repos.d/zabbix-frontend.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/zabbix-frontend.erb"),
+      }
+    }
+  }
+}
