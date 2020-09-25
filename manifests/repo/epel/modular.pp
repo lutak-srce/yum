8e9a58654c19624aec156bd6d25f168272d3d609
eimamagi <emir.imamagic@gmail.com>
2020-09-25 02:44:04 +0200

    Add Yum repo epel-modular.

---
+++ manifests/repo/epel/modular.pp
@@ -0,0 +1,24 @@
+# Class: yum::repo::epel::modular
+#
+# This module manages EPEL modular repo files for $lsbdistrelease
+#
+class yum::repo::epel::modular (
+  $priority = '11',
+  $exclude  = [],
+  $include  = [],
+) {
+  require yum::repo::epel
+
+  case $::operatingsystemrelease {
+    default: {}
+    /^8.*/: {
+      file { '/etc/yum.repos.d/epel-modular.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/epel-modular.erb"),
+      }
+    }
+  }
+}
