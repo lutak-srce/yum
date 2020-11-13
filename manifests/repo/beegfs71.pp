c15468929d8c7dac32728191e0977133e1bdc53b
Jakov Sosic <jsosic@gmail.com>
2020-11-13 12:22:49 +0100

    Refactor BeeGFS 7.1 repo

--- manifests/repo/beegfs71.pp
+++ manifests/repo/beegfs71.pp
@@ -1,19 +1,19 @@
-# Class: yum::repo::beegfs71
+#
+# = Class: yum::repo::beegfs71
 #
 # This module manages BeeGFS 7.1 repo files for $lsbdistrelease
 #
 class yum::repo::beegfs71 (
-  $exclude   = [],
-  $include   = [],
-  $debuginfo = false,
+  $exclude = [],
+  $include = [],
 ){
   require yum::repo::base
 
   file { '/etc/yum.repos.d/beegfs71.repo' :
-    ensure => file,
-    mode   => '0644',
-    owner  => root,
-    group  => root,
-    source => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/beegfs71.repo",
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template('yum/CentOS/beegfs71.erb'),
   }
 }
