d850d31ecf2f1d25f931d63b7917e8372f9428a3
Jakov Sosic <jsosic@gmail.com>
2020-11-13 12:18:04 +0100

    Refactor BeeGFS repo
    - remove files, move to template
    - detect OS version from facter

--- manifests/repo/beegfs.pp
+++ manifests/repo/beegfs.pp
@@ -1,19 +1,19 @@
-# Class: yum::repo::beegfs
+#
+# = Class: yum::repo::beegfs
 #
 # This module manages BeeGFS repo files for $lsbdistrelease
 #
 class yum::repo::beegfs (
-  $exclude   = [],
-  $include   = [],
-  $debuginfo = false,
+  $exclude = [],
+  $include = [],
 ){
   require yum::repo::base
 
   file { '/etc/yum.repos.d/beegfs.repo' :
-    ensure => file,
-    mode   => '0644',
-    owner  => root,
-    group  => root,
-    source => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/beegfs.repo",
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template('yum/CentOS/beegfs.erb'),
   }
 }
