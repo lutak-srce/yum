ab27976ebc14a64f86978c967fd2987642ab8166
ajurjevi <26479807+ajurjevi@users.noreply.github.com>
2021-12-02 09:28:11 +0100

    Add php74 support for el7

--- manifests/repo/remi/php74.pp
+++ manifests/repo/remi/php74.pp
@@ -4,15 +4,35 @@
 # This module manages Remi php74 repo files for $lsbdistrelease
 #
 class yum::repo::remi::php74(
+  $priority  = '91',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
 ) {
-  require yum::repo::remi
-  require yum::repo::remi::modular
 
-  file { '/etc/dnf/modules.d/php.module':
-    ensure  => file,
-    mode    => '0644',
-    owner   => root,
-    group   => root,
-    content => template("yum/generic/php.module.erb"),
+  case $::operatingsystemrelease {
+    default : {}
+    /^7.*/: {
+      require yum::repo::remi
+      file { '/etc/yum.repos.d/remi-php74.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/remi-php74.erb"),
+      }
+    }
+    /^8.*/: {
+      require yum::repo::remi
+      require yum::repo::remi::modular
+      file { '/etc/dnf/modules.d/php.module':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template('yum/generic/php.module.erb'),
+      }
+    }
   }
+
 }
