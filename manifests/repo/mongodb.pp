3c82648286c3df534e2045a5b1a119ecffcd6347
Jakov Sosic <jsosic@gmail.com>
2022-05-07 10:54:49 +0200

    Update mongodb repo to match upstream

--- manifests/repo/mongodb.pp
+++ manifests/repo/mongodb.pp
@@ -1,22 +1,24 @@
-# Class: yum::repo::mongodb
+#
+# = Class: yum::repo::mongodb
 #
 # This module manages MongoDB repo files for $lsbdistrelease
 #
 class yum::repo::mongodb (
   $priority  = '1',
-  $version   = '3.4',
+  $version   = '5.0',
   $exclude   = [],
   $include   = [],
+  $baseurl   = undef,
   $debuginfo = false,
 ){
   require ::yum::repo::base
 
-  file { '/etc/yum.repos.d/mongodb.repo' :
+  file { "/etc/yum.repos.d/mongodb-org-${version}.repo":
     ensure  => file,
     mode    => '0644',
     owner   => root,
     group   => root,
-    content => template("yum/generic/mongodb.erb"),
+    content => template("yum/generic/mongodb-org.erb"),
   }
 
 }
