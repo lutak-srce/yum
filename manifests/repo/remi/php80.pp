bc9b2d7da53740c71df83dd13854b01e3eff4eb4
Ante Jurjevic <ajurjevi@srce.hr>
2022-05-16 15:08:12 +0200

    Add remi repo for php80

---
+++ manifests/repo/remi/php80.pp
@@ -0,0 +1,27 @@
+#
+# = Class: yum::repo::remi::php80
+#
+# This module manages Remi php80 repo files for $lsbdistrelease
+#
+class yum::repo::remi::php80(
+  $priority  = '91',
+  $exclude   = [],
+  $include   = [],
+  $debuginfo = false,
+) {
+
+  case $::operatingsystemrelease {
+    default : {}
+    /^7.*/: {
+      require yum::repo::remi
+      file { '/etc/yum.repos.d/remi-php80.repo':
+        ensure  => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/remi-php80.erb"),
+      }
+    }
+  }
+
+}
