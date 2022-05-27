cc99de2e50faec6b5e16abd93f974405d2627132
Jakov Sosic <jsosic@gmail.com>
2022-05-27 21:25:45 +0200

    Add scylla repo

---
+++ manifests/repo/scylla.pp
@@ -0,0 +1,23 @@
+#
+# = Class: yum::repo::scylla
+#
+# This module manages Scylla repo files for $lsbdistrelease
+#
+class yum::repo::scylla (
+  $priority   = '1',
+  $exclude    = [],
+  $include    = [],
+  $enterprise = false,
+  $version    = '3.1',
+  $baseurl    = undef,
+){
+
+  file { '/etc/yum.repos.d/scylla.repo' :
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${::operatingsystem}/scylla.erb"),
+  }
+
+}
