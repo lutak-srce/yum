39f7604241f5852763937691d9c28f3a8a302ff9
Jakov Sosic <jsosic@gmail.com>
2023-06-03 02:35:15 +0200

    Update virtualbox repo to support 7.0

--- manifests/repo/virtualbox.pp
+++ manifests/repo/virtualbox.pp
@@ -1,26 +1,19 @@
-# Class: yum::repo::virtualbox
+#
+# = Class: yum::repo::virtualbox
 #
 # This module manages VirtualBox repo files for $lsbdistrelease
 #
 class yum::repo::virtualbox (
-  $exclude   = [],
-  $include   = [],
-  $debuginfo = false,
+  $exclude = [],
+  $include = [],
 ) {
-
-  exec {'vboxrepokeyimport':
-    command => '/bin/rpm --import http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
-    unless  => 'rpm -qa | grep 98ab5139 > /dev/null',
-    path    => '/bin',
-  }
 
   file { '/etc/yum.repos.d/virtualbox.repo':
     ensure  => file,
     mode    => '0644',
     owner   => root,
     group   => root,
-    source  => "puppet:///modules/yum/${::operatingsystem}/${::operatingsystemrelease}/virtualbox.repo",
-    require => Exec['vboxrepokeyimport'],
+    content => template('yum/generic/virtualbox.erb'),
   }
 
 }
