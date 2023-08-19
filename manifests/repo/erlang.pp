2ebf359779cc27f06b067b3545563f939e21a700
Jakov Sosic <jsosic@gmail.com>
2023-08-19 23:28:41 +0200

    Switch erlang repo to modern erlang
    Erlang Solutions become closed source

--- manifests/repo/erlang.pp
+++ manifests/repo/erlang.pp
@@ -1,25 +1,24 @@
 #
 # = Class: yum::repo::erlang
 #
-# This module manages erlang repo files for $operatingsystemrelease
+# This module manages modern erlang repo files for $operatingsystemrelease
 #
 class yum::repo::erlang (
-  $priority  = '1',
-  $exclude   = [],
-  $include   = [],
-  $baseurl   = undef,
+  $priority = '1',
+  $exclude  = [],
+  $include  = [],
+  $baseurl  = undef,
 ){
 
-  require ::yum::repo::base
-  require ::yum::repo::epel
-
+  require yum::repo::base
+  require yum::repo::epel
 
   file { '/etc/yum.repos.d/erlang.repo' :
     ensure  => file,
     mode    => '0644',
     owner   => root,
     group   => root,
-    content => template("yum/${::operatingsystem}/erlang.erb"),
+    content => template("yum/generic/erlang.erb"),
   }
 
 }
