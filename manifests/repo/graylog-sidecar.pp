8307db5ef12eb08347e20047b124b411453c1152
eimamagi <emir.imamagic@gmail.com>
2023-10-12 12:34:36 +0200

    Add graylog-sidecar repo.

---
+++ manifests/repo/graylog-sidecar.pp
@@ -0,0 +1,31 @@
+#
+# = Class: yum::repo::graylog-sidecar
+#
+# This module manages Graylog sidecar repo files for $lsbdistrelease
+#
+class yum::repo::graylog-sidecar (
+  $exclude   = [],
+  $include   = [],
+  $source    = false,
+  $priority  = '1',
+){
+  require ::yum::repo::base
+
+  File {
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    require => Package['graylog-sidecar-repository'],
+  }
+
+  package { 'graylog-sidecar-repository':
+    ensure   => present,
+    provider => 'rpm',
+    source   => 'https://packages.graylog2.org/repo/packages/graylog-sidecar-repository-1-5.noarch.rpm'
+  }
+
+  file { '/etc/yum.repos.d/graylog-sidecar.repo':
+    content => template('yum/generic/graylog-sidecar.erb'),
+  }
+}
