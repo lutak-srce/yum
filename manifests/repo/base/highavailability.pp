0eabe73b5918abb4a50c2659ce6e93711c3e4d21
Jakov Sosic <jsosic@gmail.com>
2023-10-18 16:47:14 +0200

    Add HighAvailability repo for Rocky 8

---
+++ manifests/repo/base/highavailability.pp
@@ -0,0 +1,36 @@
+#
+# = Class: yum::repo::base::highavailability
+#
+# This module manages highavailability repo files for $operatingsystemrelease
+#
+class yum::repo::base::highavailability (
+  $priority       = '2',
+  $exclude        = [],
+  $include        = [],
+  $baseurl        = undef,
+  $baseurl_debug  = undef,
+  $baseurl_source = undef,
+  $debuginfo      = false,
+  $source         = false,
+){
+
+  require yum::repo::base
+
+  case $::operatingsystemrelease {
+    default: {
+      $prefix = 'Rocky-HighAvailability'
+    }
+    /^8.*/: {
+      $prefix = "${facts['os']['name']}-HighAvailability"
+    }
+  }
+
+  file { "/etc/yum.repos.d/${prefix}.repo":
+    ensure  => file,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    content => template("yum/${facts['os']['name']}/${facts['os']['release']['full']}/${prefix}.erb"),
+  }
+
+}
