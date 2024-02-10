7b65b09af53ff5c5649f18083d5d8af0f35465fb
Jakov Sosic <jsosic@gmail.com>
2024-02-10 11:06:48 +0100

    Update zfs to use new release and new repo
    Keep backwards compatibility with older CentOS versions

--- manifests/repo/zfs.pp
+++ manifests/repo/zfs.pp
@@ -4,13 +4,14 @@
 # This class manages ZFS repo files for $lsbdistrelease
 #
 class yum::repo::zfs (
-  $priority  = '99',
-  $exclude   = [],
-  $include   = [],
-  $baseurl   = undef,
-  $debuginfo = false,
-  $source    = false,
-  $kmod      = false,
+  $priority     = '99',
+  $exclude      = [],
+  $include      = [],
+  $baseurl      = undef,
+  $baseurl_kmod = undef,
+  $debuginfo    = false,
+  $source       = false,
+  $kmod         = false,
 ) {
   require ::yum::repo::epel
 
@@ -19,40 +20,14 @@ class yum::repo::zfs (
     mode    => '0644',
     owner   => root,
     group   => root,
-    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/zfs.erb"),
+    content => template("yum/${facts['os']['name']}/${facts['os']['release']['full']}/zfs.erb"),
     require => Package['zfs-release'],
   }
 
-  # install package depending on major version
-  case $::operatingsystemrelease {
-    default: {}
-    /^6.*/: {
-      package { 'zfs-release':
-        ensure   => present,
-        provider => 'rpm',
-        source   => 'http://download.zfsonlinux.org/epel/zfs-release.el6.noarch.rpm',
-      }
-    }
-    /^7.*/: {
-      package { 'zfs-release':
-        ensure   => present,
-        provider => 'rpm',
-        source   => "http://download.zfsonlinux.org/epel/zfs-release.el7_${facts['os']['release']['minor']}.noarch.rpm",
-      }
-    }
-    /^8.*/: {
-      package { 'zfs-release':
-        ensure   => present,
-        provider => 'rpm',
-        source   => "http://download.zfsonlinux.org/epel/zfs-release.el8_${facts['os']['release']['minor']}.noarch.rpm",
-      }
-    }
-    /^9.*/: {
-      package { 'zfs-release':
-        ensure   => present,
-        provider => 'rpm',
-        source   => "https://zfsonlinux.org/epel/zfs-release-2-2.el9.noarch.rpm",
-      }
-    }
+  package { 'zfs-release':
+    ensure   => present,
+    provider => 'rpm',
+    source   => "https://zfsonlinux.org/epel/zfs-release-2-3.el${facts['os']['release']['major']}.noarch.rpm",
   }
+
 }
