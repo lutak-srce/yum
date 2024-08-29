f0f38fd46d523363c076c2de0061f0bae48b08c3
Jakov Sosic <jsosic@gmail.com>
2024-08-29 15:04:40 +0200

    Fix epel repo for EOL-ed CentOS 7

--- manifests/repo/epel.pp
+++ manifests/repo/epel.pp
@@ -16,14 +16,20 @@ class yum::repo::epel (
     mode    => '0644',
     owner   => root,
     group   => root,
-    content => template("yum/${::operatingsystem}/${::operatingsystemrelease}/epel.erb"),
+    content => template("yum/${facts['os']['name']}/${facts['os']['release']['full']}/epel.erb"),
     require => Package['epel-release'],
+  }
+
+  $release_package = $facts['os']['release']['major'] ? {
+    '6'     => 'https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/Packages/e/epel-release-6-8.noarch.rpm',
+    '7'     => 'https://archives.fedoraproject.org/pub/archive/epel/7/x86_64/Packages/e/epel-release-7-14.noarch.rpm',
+    default => "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${facts['os']['release']['major']}.noarch.rpm",
   }
 
   # install package depending on major version
   package { 'epel-release':
     provider => 'rpm',
-    source   => "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${facts['os']['release']['major']}.noarch.rpm",
+    source   => $release_package,
   }
 
 }
