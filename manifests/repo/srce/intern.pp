3a17caa2595d68e855255c2b069d7a6c5899d9fb
Mihael Burlic <mburlic@srce.hr>
2025-09-25 17:03:52 +0200

    Add srcerelease variable in srce.pp

--- manifests/repo/srce/intern.pp
+++ manifests/repo/srce/intern.pp
@@ -22,9 +22,13 @@ class yum::repo::srce::intern (
   }
 
   # install package depending on major version
+  case $facts['os']['release']['full'] {
+    /^10.*/: { $srcerelease = '6-0' }
+    default: { $srcerelease = '5-3' }
+  }
   package { 'srce-release-intern' :
     provider => 'rpm',
-    source   => "http://ftp.srce.hr/srce-redhat/base/el${facts['os']['release']['major']}/x86_64/srce-release-intern-5-3.el${facts['os']['release']['major']}.srce.noarch.rpm",
+    source   => "http://ftp.srce.hr/srce-redhat/base/el${facts['os']['release']['major']}/x86_64/srce-release-intern-${srcerelease}.el${facts['os']['release']['major']}.srce.noarch.rpm",
     require  => Package['srce-release'],
   }
 }
